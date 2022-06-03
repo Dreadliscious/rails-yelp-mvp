b34f0d5ec85d981642e3e192c963bfcc53fb758c
damien desparain <damien.cpielerins@gmail.com>
2022-06-03 11:16:41 +0200

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Cleaning database..."
-Restaurant.destroy_all
-
-puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "japanese" }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
-one = { name: "one", address: "7 Boundary St, London E2 7JE", category: "chinese" }
-two =  { name: "two", address: "56A Shoreditch High St, London E1 6PQ", category: "chinese" }
-tree = { name: "tree", address: "7 Boundary St, London E2 7JE", category: "chinese" }
-
-
-[ dishoom, pizza_east, one, two, tree ].each do |attributes|
-  restaurant = Restaurant.create!(attributes)
-  puts "Created #{restaurant.name}"
-end
-puts "Finished!"
